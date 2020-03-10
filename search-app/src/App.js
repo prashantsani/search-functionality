c8b105ba594730d3988a754cf4a7bfaf6441f04f
Prashant Sani <contact@prashantsani.com>
2020-03-10 16:33:25 +0530

    using compoenent state to show the SELECTED BOOKS (which are rendered using <Card /> component) + adding a click handler which searches and console.logs the data using the search-utility-function created earlier

--- search-app/src/App.js
+++ search-app/src/App.js
@@ -1,14 +1,43 @@
 import React, { Component } from 'react';
 import './App.css';
 import Card from './components/Card.js';
+import data from './data.json';
+import search from './search-utility-function.js';
 
 class App extends Component {
-  render() {
-    var temp_obj = {
-      title: "The Richest Man in Babylon",
-      summary: "The Book in Three Sentences: The 10X Rule says that 1) you should set targets for yourself that are 10X greater than what you believe you can achieve and 2) you should take actions that are 10X greater than what you believe are necessary to achieve your goals. The biggest mistake most people make in life is not setting goals high enough. Taking massive action is the only way to fulfill your true potential",
-      author:"Grant Cardone"
+  constructor(props){
+    super(props);
+    this.state = {
+      'selected_but_not_added': null,
+      'selected_books':[
+        {
+          title: "The Richest Man in Babylon",
+          summary: "The Book in Three Sentences: The 10X Rule says that 1) you should set targets for yourself that are 10X greater than what you believe you can achieve and 2) you should take actions that are 10X greater than what you believe are necessary to achieve your goals. The biggest mistake most people make in life is not setting goals high enough. Taking massive action is the only way to fulfill your true potential",
+          author:"Grant Cardone",
+          id:"1"
+        },
+        {
+          title: "The Richest Man in Babylon",
+          summary: "The Book in Three Sentences: The 10X Rule says that 1) you should set targets for yourself that are 10X greater than what you believe you can achieve and 2) you should take actions that are 10X greater than what you believe are necessary to achieve your goals. The biggest mistake most people make in life is not setting goals high enough. Taking massive action is the only way to fulfill your true potential",
+          author:"Grant Cardone",
+          id:"2"
+        }
+      ]
     }
+  }
+  handleKeyDown(e){
+    // Only search if the value is more than 4 letters
+    if(e.target.value.length>4){
+      var results = search(e.target.value,3, data);
+      console.log(results);
+    }
+  }
+  render() {
+    const cards = this.state.selected_books.map(function(book){
+      return <Card key={book.id} title={book.title} summary={book.summary} author={book.author}/>;
+    });
+
+
     return (
       <div className="App">
         <div className="App-header">
@@ -16,17 +45,14 @@ class App extends Component {
         </div>
         
         <form id="user-search" className="user-search">
-          <input type="text" className="user-search__input" />
-          <button className="user-search__search">SUBMIT</button>
+          <input type="text" className="user-search__input" onKeyDown={this.handleKeyDown} />
+          <button className="user-search__search">SELECT</button>
           <h4 className="text-center" className="text-center user-search__selected-book">No Books Selected</h4>
         </form>
 
         <div className="card-wrapper">
-          <Card title={temp_obj.title} summary={temp_obj.summary} author={temp_obj.author}/>
-          <Card title={temp_obj.title} summary={temp_obj.summary} author={temp_obj.author}/>
-          <Card title={temp_obj.title} summary={temp_obj.summary} author={temp_obj.author}/>
+          {cards}
         </div>
-
       </div>
     );
   }
---
+++ search-app/src/search-utility-function.js
@@ -0,0 +1,87 @@
+function search_summaries(query,k,j){
+  // Input: 
+  // The input should be a ​string query (query) {'is​ your problems' }
+  // ​and​ number ​of items to ​return(k) {eg. ​3}
+  // j refers to JSON querry
+  var relevant_matches=[],
+      all_matches =[];
+
+
+  // This is basic type and data check 
+  if( query ==='' || query === undefined || query === null || typeof(query)!=='string'){
+    return 'Please enter a search query'
+  }
+
+  if(typeof(k)!=='number'){
+    return 'Please enter number of items in number'
+  }
+
+  if(!j.hasOwnProperty('titles') || !j.hasOwnProperty('queries') || !j.hasOwnProperty('summaries') || !j.hasOwnProperty('authors')){
+    return 'Incorrect JSON'
+  }
+
+  if(j.titles.length ===0 || j.queries.length ===0 || j.summaries.length ===0 || j.authors.length ===0 ){
+    return 'Please add more values to JSON'
+  }
+  // End of type/data check
+
+
+
+
+  
+  // ------------------------------------------------------------------
+  // LOGIC 
+
+  // Find the 
+  let query_lc = query.toLowerCase();
+  all_matches = j.summaries.filter(summary => summary.summary.toLowerCase().includes(query_lc.toLowerCase()));
+
+  relevant_matches = all_matches;
+
+  // Do search only of the output (relevant_matches) is less than 
+  // the expected number of results
+  
+  // So, it first matches the exact text
+  // if not it matches the words of the input 
+  if(relevant_matches.length < k){
+    // Search for each word
+    // Ignore the words a, is, the, who, etc
+    let each_word_search = query.split(' ');
+    each_word_search = each_word_search.filter(val => (val!=='a' & val!=='is' & val!=='your' & val!=='mine' & val!=='her' & val!=='his' & val!=='the' & val!=='for' & val!=='for' ) )
+
+
+    for(let a = 0; a < each_word_search.length; a++){
+      console.log(each_word_search[a])
+      all_matches = j.summaries.filter(summary => summary.summary.toLowerCase().includes(each_word_search[a]));
+    }
+
+    relevant_matches = all_matches;
+  }
+
+  // If more matches than expected(k) are found, use Array.slice(start,end);
+  // Else just return all matches
+  if(all_matches.length > k){
+    relevant_matches = all_matches.slice(0,k);
+  }else{
+    relevant_matches = all_matches;
+  }
+  
+  // LOGIC END
+  // ------------------------------------------------------------------
+
+
+
+
+  // Return statements
+  if(relevant_matches.length===0){
+    return 'No items found';
+  }
+
+  // Output:
+  // List ​of​ K relevant summaries sorted according to order ​of relevance
+  // A summary ​is​ a dictionary that follows the schema:-
+  // {​'summary​': string, ​'id​': integer}
+  return relevant_matches;
+}
+
+module.exports = search_summaries;
