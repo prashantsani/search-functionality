0c1a25cfc2a5803218fa867ec1dd8e67f6f8b362
Prashant Sani <contact@prashantsani.com>
2020-03-10 12:07:14 +0530

    creating a new js file and adding input and output values for that function, adding a return string incase if the input values are incorrect

---
+++ search-utility-function/search-utility-function.js
@@ -0,0 +1,21 @@
+function search_summaries(query,k){
+  // Input: 
+  // The input should be a ​string query (query) {'is​ your problems' }
+  // ​and​ number ​of items to ​return(k) {eg. ​3}
+  var relevant_matches =[],
+      all_matches;
+
+  if( query ==='' || query === undefined || query === null || typeof(query)!=='string'){
+    return 'Please enter a search query'
+  }
+
+  if(typeof(k)!=='number'){
+    return 'Please enter number of items in number'
+  }
+
+  // Output:
+  // List ​of​ K relevant summaries sorted according to order ​of relevance
+  // A summary ​is​ a dictionary that follows the schema:-
+  // {​'summary​': string, ​'id​': integer}
+  return relevant_matches;
+}
